431d85e28bb2f0ad7997b022f6b1f79f6830de2a
Manjeera <manjeera.thogarcheti@gmail.com>
2021-06-26 11:17:20 -0400

    Week-4-assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-
+// ******************** script for having the play and pause button simultaneously on carousel *************************
      $(function() {
         $(".carousel").carousel( { interval: 2000, pause: "false" } );
         $("#carouselButton").click(function() {
-            // **********if the class with id carouselButton has a child tag (i) and that child has a class fa-pause then perform the code below*****************
+            //if the class with id carouselButton has a child tag (i) and that child has a class fa-pause then perform the code below
             if($("#carouselButton").children("i").hasClass("fa-pause")) {
                     $(".carousel").carousel("pause") ;  //pause the carousal using bootstrap jquery method .carousel("pause")//
                     $("#carouselButton").children("i").removeClass("fa-pause"); // remove the child class fa-pause - the removeClass method is Javascript
                     $("#carouselButton").children("i").addClass("fa-play"); //Add the class fa-play to the child of carousalButton id div
             }
-            // **********if the class with id carouselButton has a child tag (i) and that child has a class fa-play then perform the code below*****************
+            //if the class with id carouselButton has a child tag (i) and that child has a class fa-play then perform the code below
             else {
                 $(".carousel").carousel("cycle"); //Bootstrap jQuery method carousel("cycle") to keep the carousel going
                 $("#carouselButton").children("i").removeClass("fa-play"); //Remove the fa-play button class
@@ -18,4 +18,16 @@
         });
         
      });
- 
+
+// ******************** activate the Reserve Campsite and Login modals when the corresponding button is clicked *************************
+     $(function() {
+        $("#reserveButton").click(function() {
+            $("#reserveModal").modal("show");
+        });        
+     });
+
+     $(function() {
+         $("#loginButton").click(function() {
+             $("#loginModal").modal("show");
+         });
+     });
